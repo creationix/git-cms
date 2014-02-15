6fe35826c42cf418e748a403ee5d91960fcd7bcc
Shepazu <schepers@w3.org>
2014-02-15 18:56:36 -0500

    starting hello-world page generator

--- filters/page-generator.js
+++ filters/page-generator.js
@@ -1,6 +1,25 @@
-module.exports = compiler;
+"use strict";
+
+var mine = require("mine");
+var pathJoin = require("pathjoin");
+var binary = require('bodec');
+
+module.exports = page_generator;
 
 
-function compiler(servePath, req, callback) {
-  callback(new Error("TODO: Implement compiler"));
+function page_generator(servePath, req, callback) {
+  var etag = req.current;
+
+  var modules = {};  // compiled modules
+  var packagePaths = {}; // key is base + name , value is full path
+  var aliases = {}; // path aliases from the "browser" directive in package.json
+
+  return callback(null, {etag: etag, fetch: fetch});
+
+  function fetch(callback) {
+	}
+
+  // callback(new Error("TODO: Implement compiler"));
 }
+
+
